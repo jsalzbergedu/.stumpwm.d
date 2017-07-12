1d5109e76f56ea85158324082a2ac58b076d0362
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-07-12 15:54:38 -0400

    Clean up, change C-t e and C-t C-e to reflect my normal usage of emacs

--- keybindings.lisp
+++ keybindings.lisp
@@ -4,4 +4,5 @@
 (define-key *root-map* (kbd "d") "exec firefox")
 (define-key *root-map* (kbd "c") "exec urxvtc")
 (define-key *root-map* (kbd "C-c") "exec urxvtc")
-
+(define-key *root-map* (kbd "e") "exec urxvtc -e emacsclient -nw")
+(define-key *root-map* (kbd "C-e") "exec urxvtc -e emacsclient -nw")
--- stumpinit.sh
+++ stumpinit.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
-# /usr/bin/compton -b  --config ~/.config/compton/compton.conf --xrender-sync --xrender-sync-fence --vsync none
-pushd ~/.stumpwm.d/ > /dev/null
+pushd /home/jacob/.stumpwm.d/ > /dev/null
 /usr/bin/compton -b  --config ./compton.conf --xrender-sync --xrender-sync-fence --vsync none
 /usr/bin/xrdb -merge ./.Xresources
 /usr/bin/urxvtd --quiet --opendisplay --fork
-# /usr/bin/bash ~/.config/lemonbar/config.sh | lemonbar -dp -f "FiraMono" -f 'WifiStatus2' & disown
 /usr/bin/bash ./lemonbar.sh | lemonbar -dp -f "FiraMono" -f 'WifiStatus2' & disown
 # This is a hack that assumes the first window is stumpwm's messages. Be careful!
 # MESSAGE_WINDOW=$(xwininfo -root -children | grep -m 3 "(has no name)" | tail -n 1 | sed "s/(has no name).\+//" | sed "s/\s\+//")
 # compton-trans -w $MESSAGE_WINDOW -o 90
-~/.stumpwm.d/day-night-toggle.sh "INIT" & disown
+/home/jacob/.stumpwm.d/day-night-toggle.sh "INIT" & disown
+/usr/bin/emacs --daemon
