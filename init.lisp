846e253fce34023035356f942769800dce7f3005
Jacob Salzberg <jssalzbe@ncsu.edu>
2019-06-27 17:22:45 -0400

    Increase opacity

--- init.lisp
+++ init.lisp
@@ -18,10 +18,12 @@
 ;; Themeing
 (setf *window-border-style* :none)
 (load-module "ttf-fonts")
-;; (set-font (make-instance 'xft:font
-;;                          :family "SauceCodePro Nerd Font"
-;;                          :subfamily "Regular"
-;;                          :size 12))
+(clx-truetype:cache-fonts)
+(set-font (make-instance 'xft:font
+	   :family "Fira Mono"
+	   :subfamily "Bold"
+	   :size 12
+	   :antialias t))
 
 ;; Gaps
 (load-module "swm-gaps")
@@ -41,7 +43,26 @@
 
 (define-key *root-map* (kbd "d") "exec firefox")
 
+(define-key *root-map* (kbd "e") "exec emacsclient -c")
+(define-key *root-map* (kbd "C-e") "exec emacsclient -c")
 (define-key *root-map* (kbd "c") "exec kitty")
 (define-key *root-map* (kbd "C-c") "exec kitty")
 
 (run-shell-command "~/.swapcaps.sh")
+(run-shell-command "compton")
+
+
+(setf *message-window-gravity* :center
+      *input-window-gravity* :center
+      *window-border-style* :none
+      *message-window-padding* 0
+      *maxsize-border-width* 0
+      *normal-border-width* 0
+      *transient-border-width* 0
+      stumpwm::*float-window-border* 2
+      stumpwm::*float-window-title-height* 5
+      *mouse-focus-policy* :click)
+
+(set-fg-color "#4d4d4c")
+(set-bg-color "#ffffff")
+(set-border-color "#d9d9d9")
